4f5bae6e600fa028a044e2a43451d6acf8ac5b22
Noman Shah <nomankhalidofficial@gmail.com>
2025-03-19 16:34:21 +0500

    Remaining  image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 
   webpack(config) {
-    
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
-
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
